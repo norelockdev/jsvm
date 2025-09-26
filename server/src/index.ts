b75b84b840725fcd627f58f3c2413885bd95e85b
norelock <business.norelock@gmail.com>
2025-09-26 22:27:49 +0200

    jsvm init

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import apiRouter from './routes/Api';
+import path from 'path';
+
+const app = express();
+
+app.use(express.json({
+  limit: "1mb"
+}));
+
+app.use('/api', apiRouter);
+
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
+
+app.listen(3000);
+console.log('listenin')
---
+++ server/src/routes/Api.ts
@@ -0,0 +1,72 @@
+import express from 'express';
+import * as Babel from "@babel/standalone";
+import terser from "terser";
+import Compiler from "../Compiler/Compiler";
+
+function transpileToEs5(code: string): string {
+  return Babel.transform(code, { presets: ['env'] }).code;
+}
+
+const apiRouter = express.Router();
+
+const allowedAuthorizationKeys = [
+  'debefd54-7f23-475f-9598-5bfe6950033b'
+];
+
+const vaildateAuthKey = (header: string): boolean => {
+  if (!header || typeof header !== 'string')
+    return false;
+
+  const [method, value] = header.split(' ');
+  if (method !== 'Basic')
+    return false;
+
+  for (let i = 0; i < allowedAuthorizationKeys.length; i++) {
+    const key = allowedAuthorizationKeys[i];
+    if (key === value)
+      return true;
+  }
+
+  return false;
+};
+
+apiRouter.post('/compile', async (req, res) => {
+  // const authHeader = req.headers.authorization ?? null;
+  // const vaild = vaildateAuthKey(authHeader);
+  // if (!vaild)
+  //   return res.status(401).json({error: "invalid authorization key"});
+
+  const { code, is_production } = req.body;
+  if (!code) return res.sendStatus(401);
+
+  try {
+    const es5Code = transpileToEs5(
+      ` (function NORELOCK_JSVM(){
+          ${code}
+        })()`
+    );
+
+    const minifiedCode = await terser.minify(es5Code, {
+      compress: {
+        defaults: true,
+        reduce_funcs: false,
+        passes: 10,
+      }
+    });
+
+    const isProduction = is_production ?? true;
+    const finalCode = isProduction ? minifiedCode.code : es5Code;
+
+    const compiler = new Compiler(finalCode, { compress: true });
+    compiler.compile().then(code => {
+      console.log(compiler.logs.join('\n'));
+
+      return res.json({ code: code[0] });
+    })
+
+  } catch (err) {
+    return res.status(501).json({ error: ("" + err).slice(0, 200) });
+  }
+});
+
+export default apiRouter;
